a1ff241e4b7dc98de24373e9cc0245e19111f7b9
Pete Cruz <iPetesta@gmail.com>
2017-09-02 21:48:12 -0700

    Add endpoints for Tumblr and case classes for data.

---
+++ build.sbt
@@ -0,0 +1,34 @@
+name := "scala-tumblr"
+
+scalaVersion := "2.11.8"
+
+resolvers += Resolver.sonatypeRepo("public")
+
+val circe = "io.circe"
+val circeVersion = "0.8.0"
+
+libraryDependencies ++= Seq(
+  "ch.qos.logback" % "logback-classic" % "1.1.7",
+  "com.twitter" %% "finagle-http" % "7.0.0",
+  circe %% "circe-core" % circeVersion,
+  circe %% "circe-generic" % circeVersion,
+  circe %% "circe-java8" % circeVersion,
+  circe %% "circe-parser" % circeVersion
+)
+
+scalacOptions ++= Seq(
+  "-Xfatal-warnings",
+  "-Xfuture",
+  "-Yno-adapted-args",
+  "-Ywarn-dead-code",
+  "-Ywarn-numeric-widen",
+  "-Ywarn-unused",
+  "-Ywarn-value-discard",
+  "-encoding", "UTF-8",
+  "-feature",
+  "-deprecation",
+  "-language:existentials",
+  "-language:higherKinds",
+  "-language:implicitConversions",
+  "-unchecked"
+)
---
+++ src/main/scala/scala-tumblr/TumblrData.scala
@@ -0,0 +1,144 @@
+package io.github.petesta.tumblr
+
+final case class Meta(
+  status: Int,
+  msg: String
+)
+
+sealed trait Response
+
+final case class TaggedResponse(
+  blog: BlogInfo
+)
+
+final case class BlogInfo(
+  ask: Boolean,
+  askAnon: Boolean,
+  askPageTitle: String,
+  canSubscribe: Boolean,
+  description: String,
+  isAdult: Boolean,
+  isNsfw: Boolean,
+  name: String,
+  posts: Long,
+  replyConditions: String,
+  shareLikes: Boolean,
+  submissionPageTitle: String,
+  subscribed: Boolean,
+  title: String,
+  totalPosts: Long,
+  updated: Long,
+  url: String,
+  isOptOutAds: Boolean
+)
+
+final case class LikeResponse(
+  blog: BlogInfo
+) extends Response
+
+final case class Reblog(
+  comment: String,
+  treeHtml: String
+)
+
+final case class Theme(
+  headerFullWidth: Long,
+  headerFullHeight: Long,
+  headerFocusWidth: Long,
+  headerFocusHeight: Long,
+  avatarShape: String,
+  backgroundColor: String,
+  bodyFont: String,
+  headerBounds: String,
+  headerImage: String,
+  headerImageFocused: String,
+  headerImageScaled: String,
+  headerStretch: Boolean,
+  linkColor: String,
+  showAvatar: Boolean,
+  showDescription: Boolean,
+  showHeaderImage: Boolean,
+  showTitle: Boolean,
+  titleColor: String,
+  titleFont: String,
+  titleFontWeight: String
+)
+
+final case class BlogObject(
+  name: String,
+  active: Boolean,
+  theme: Theme,
+  shareLikes: Boolean,
+  shareFollowing: Boolean,
+  canBeFollowed: Boolean
+)
+
+final case class PostObject(
+  id: String
+)
+
+final case class Trail(
+  blog: List[BlogObject],
+  post: PostObject,
+  contentRaw: String,
+  content: String,
+  isCurrentItem: Boolean,
+  isRootItem: Boolean
+)
+
+final case class Size(
+  url: String,
+  width: Long,
+  height: Long
+)
+
+final case class Photo(
+  caption: String,
+  originalSize: Size,
+  altSizes: List[Size]
+)
+
+final case class Post(
+  blogName: String,
+  id: Long,
+  postUrl: String,
+  slug: String,
+  date: String, // ZonedDateTime
+  timestamp: Long,
+  state: String,
+  format: String,
+  reblogKey: String,
+  tags: List[String],
+  shortUrl: String,
+  summary: String,
+  isBlocksPostFormat: Boolean,
+  // recommendedSource: Option[],
+  // recommendedColor: Option[],
+  postAuthor: String,
+  postAuthorIsAdult: Boolean,
+  isSubmission: Boolean,
+  noteCount: Long,
+  title: String,
+  body: String,
+  reblog: Reblog,
+  trail: List[Trail],
+  photosetLayout: String,
+  photos: Option[List[Photo]],
+  canLike: Boolean,
+  canReblog: Boolean,
+  canSendInMessage: Boolean,
+  canReply: Boolean,
+  displayAvatar: Boolean
+)
+
+final case class PostResponse(
+  blog: BlogObject,
+  posts: List[Post],
+  totalPosts: Long// ,
+  // supplyLoggingPositions: List[]
+)
+
+final case class TumblrResponse(
+  meta: Meta,
+  response: Response
+)
