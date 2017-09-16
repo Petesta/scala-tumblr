b7f9669c2d2376b09a22e1ba63139e074779e088
Pete Cruz <iPetesta@gmail.com>
2017-09-16 00:26:07 -0700

    Split up endpoints.

--- src/main/scala/scala-tumblr/endpoints/Oauth.scala
+++ src/main/scala/scala-tumblr/endpoints/Oauth.scala
@@ -1,44 +1,5 @@
 package io.github.petesta.tumblr
 
-import com.twitter.finagle.http.{Method, Request}
-
-final case class Avatar(blogName: String, size: Int) extends Tumblr {
-  val path = s"${versionBlog(blogName)}/avatar"
-  val params: Option[Map[String, String]] = None
-
-  def get = client(Request(Method.Get, s"$root$path?size=$size"))
-}
-
-final case class Info(
-  blogName: String,
-  params: Option[Map[String, String]] = None
-)(implicit val noOauthConfig: NoOauthConfig) extends ApiKey {
-  val path = s"${versionBlog(blogName)}/info"
-}
-
-final case class Likes(
-  blogName: String,
-  params: Option[Map[String, String]] = None
-)(implicit val noOauthConfig: NoOauthConfig) extends ApiKey {
-  val path = s"${versionBlog(blogName)}/likes"
-}
-
-final case class Posts(
-  blogName: String,
-  params: Option[Map[String, String]] = None
-)(implicit val noOauthConfig: NoOauthConfig) extends ApiKey {
-  val path = s"${versionBlog(blogName)}/posts"
-}
-
-final case class Tagged(
-  tag: String,
-  params: Option[Map[String, String]] = None
-)(implicit val noOauthConfig: NoOauthConfig) extends ApiKey {
-  val path = "/v2/tagged"
-
-  override def get = client(Request(Method.Get, s"$root$path$apiKey&tag=$tag$keyValuePairs"))
-}
-
 final case class Following(
   blogName: String,
   params: Option[Map[String, String]] = None
