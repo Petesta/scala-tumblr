2a7cc83acedcd50d9b3b5bdb8d3186b89bcef478
Pete Cruz <iPetesta@gmail.com>
2017-09-25 13:46:04 -0700

    Optional parameter.

--- src/main/scala/scala-tumblr/api/NoCreds.scala
+++ src/main/scala/scala-tumblr/api/NoCreds.scala
@@ -1,6 +1,6 @@
 package io.github.petesta.tumblr
 
-final case class Avatar(blogName: String, size: Int) extends Tumblr {
+final case class Avatar(blogName: String, size: Option[Int] = None) extends Tumblr {
   val path = s"${versionBlog(blogName)}/avatar"
-  protected val url = s"$root$path?size=${size.toString}"
+  protected val url = s"$root$path${size.map("?size=" + _.toString).getOrElse("")}"
 }
